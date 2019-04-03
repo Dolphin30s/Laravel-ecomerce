07343e5f2b35abb53a813bfbeed84a8a7afb215c
unknown <nazmus.shakib.cse@gmail.com>
2019-04-03 17:52:20 +0600

    Initial commit

---
+++ resources/views/layouts/partials/scripts.blade.php
@@ -0,0 +1,9 @@
+<!--=============================================
+=            JS files        =
+=============================================-->
+
+<!-- Vendor JS -->
+<script src="{{asset('assets/js/vendors.js')}}"></script>
+<!-- Active JS -->
+<script src="{{asset('assets/js/active.js')}}"></script>
+<!--=====  End of JS files ======-->
---
+++ resources/views/layouts/partials/styles.blade.php
@@ -0,0 +1,8 @@
+<!--=============================================
+    =            CSS  files       =
+    =============================================-->
+
+<!-- Vendor CSS -->
+<link href="{{asset('assets/css/vendors.css')}}" rel="stylesheet">
+<!-- Main CSS -->
+<link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
